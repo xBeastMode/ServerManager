1d6dee030b29fba55c1a3ee6ca8517221662226b
Amir Emad <amir.emad9@gmail.com>
2016-08-20 09:52:15 -0700

    Update ServerManager.php

--- src/mcrafters/ServerManager.php
+++ src/mcrafters/ServerManager.php
@@ -64,143 +64,177 @@ class ServerManager extends PluginBase implements Listener
 
     public function onPlace(BlockPlaceEvent $bpe)
     {
-        if($this->cfg->get("BlockPlacing") == false or $this->cfg->get("BlockPlacing") == disabled){
+        if($this->cfg->get("BlockPlacing") == false or $this->cfg->get("BlockPlacing") == disable){
             if(!$bpe->getPlayer()->hasPermission('servermanager.bypass')){
-                $bpe->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("BlockPlacing")));
+                if(!$this->cfgm->get("BlockPlacing") == null) {
+                    $bpe->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("BlockPlacing")));
+                }
                 $bpe->setCancelled();
             }
         }
     }
     public function onBreak(BlockBreakEvent $bbe)
     {
-        if($this->cfg->get("BlockBreaking") == false or $this->cfg->get("BlockBreaking") == disabled){
+        if($this->cfg->get("BlockBreaking") == false or $this->cfg->get("BlockBreaking") == disable){
             if(!$bbe->getPlayer()->hasPermission('servermanager.bypass')){
-                $bbe->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("BlockBreaking")));
-                $bbe->setCancelled();
+                if(!$this->cfgm->get("BlockBreaking") == null) {
+                    $bbe->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("BlockBreaking")));
+                }
+                    $bbe->setCancelled();
             }
         }
     }
     public function onChat(PlayerChatEvent $pce)
     {
-        if($this->cfg->get("Chatting") == false or $this->cfg->get("Chatting") == disabled){
+        if($this->cfg->get("Chatting") == false or $this->cfg->get("Chatting") == disable){
             if(!$pce->getPlayer()->hasPermission('servermanager.bypass')){
-                $pce->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Chatting")));
-                $pce->setCancelled();
+                if(!$this->cfgm->get("Chatting") == null) {
+                    $pce->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Chatting")));
+                }
+                    $pce->setCancelled();
             }
         }
     }
     public function onDropItem(PlayerDropItemEvent $pdie)
     {
-        if($this->cfg->get("ItemDrop") == false or $this->cfg->get("ItemDrop") == disabled){
+        if($this->cfg->get("ItemDrop") == false or $this->cfg->get("ItemDrop") == disable){
             if(!$bbe->getPlayer()->hasPermission('servermanager.bypass')){
-                $bbe->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("ItemDrop")));
-                $bbe->setCancelled();
+                if(!$this->cfgm->get("ItemDrop") == null) {
+                    $bbe->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("ItemDrop")));
+                }
+                    $bbe->setCancelled();
             }
         }
     }
     public function onDeath(PlayerDeathEvent $pde)
     {
-        if($this->cfg->get("Death") == false or $this->cfg->get("Death") == disabled){
-            if(!$bbe->getPlayer()->hasPermission('servermanager.bypass')){
+        if($this->cfg->get("Death") == false or $this->cfg->get("Death") == disable){
+            if(!$bbe->getPlayer()->hasPermission('servermanager.bypass')) {
+                if(!$this->cfgm->get("Death") == null) {
                 $bbe->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Death")));
+            }
                 $bbe->setCancelled();
             }
         }
     }
     public function onGameModeChange(PlayerGameModeChangeEvent $pgmce)
     {
-        if ($this->cfg->get("GameModeChange") == false or $this->cfg->get("GameModeChange") == disabled) {
+        if ($this->cfg->get("GameModeChange") == false or $this->cfg->get("GameModeChange") == disable) {
             if (!$pgmce->getPlayer()->hasPermission('servermanager.bypass')) {
-                $pgmce->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("GameModeChange")));
-                $pgmce->setCancelled();
+                if(!$this->cfgm->get("GameModeChange") == null) {
+                    $pgmce->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("GameModeChange")));
+                }
+                    $pgmce->setCancelled();
             }
         }
     }
 
     public function onCommandPreprocess (PlayerCommandPreprocessEvent $pcpe){
-        if ($this->cfg->get("Commands") == false or $this->cfg->get("Commands") == disabled) {
+        if ($this->cfg->get("Commands") == false or $this->cfg->get("Commands") == disable) {
             if (!$cpe->getPlayer()->hasPermission('servermanager.bypass')) {
-                $pcpe->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Commands")));
-                $pcpe->setCancelled();
+                if(!$this->cfgm->get("Commands") == null) {
+                    $pcpe->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Commands")));
+                }
+                    $pcpe->setCancelled();
             }
         }
     }
     public function onInteract (PlayerInteractEvent $pie){
-        if ($this->cfg->get("PVP") == false or $this->cfg->get("PVP") == disabled) {
+        if ($this->cfg->get("PVP") == false or $this->cfg->get("PVP") == disable) {
             if (!$pie->getPlayer()->hasPermission('servermanager.bypass')) {
-                $pie->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("PVP")));
-                $pie->setCancelled();
+                if(!$this->cfgm->get("PVP") == null) {
+                    $pie->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("PVP")));
+                }
+                    $pie->setCancelled();
             }
         }
     }
     public function onItemConsume (PlayerItemConsumeEvent $pice){
-        if ($this->cfg->get("Item Damage") == false or $this->cfg->get("Item Damage") == disabled) {
+        if ($this->cfg->get("Item Damage") == false or $this->cfg->get("Item Damage") == disable) {
             if (!$pice->getPlayer()->hasPermission('servermanager.bypass')) {
-                $pice->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Item Damage")));
-                $pice->setCancelled();
+                if(!$this->cfgm->get("Item Damage") == null) {
+                    $pice->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Item Damage")));
+                }
+                    $pice->setCancelled();
             }
         }
     }
     public function onKick (PlayerKickEvent $pke){
-        if ($this->cfg->get("Player Kicking") == false or $this->cfg->get("Player Kicking") == disabled) {
+        if ($this->cfg->get("Player Kicking") == false or $this->cfg->get("Player Kicking") == disable) {
             if (!$pke->getPlayer()->hasPermission('servermanager.bypass')) {
-                $pke->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Player Kick")));
+                if(!$this->cfgm->get("Player Kicking") == null) {
+                    $pke->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Player Kick")));
+                }
                 $pke->setCancelled();
             }
         }
     }
     public function onRespawn (PlayerRespawnEvent $pre){
-        if ($this->cfg->get("Player Respawning") == false or $this->cfg->get("Player Respawning") == disabled) {
+        if ($this->cfg->get("Player Respawning") == false or $this->cfg->get("Player Respawning") == disable) {
             if (!$pre->getPlayer()->hasPermission('servermanager.bypass')) {
-                $pre->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Player Respawning")));
-                $pre->setCancelled();
+                if(!$this->cfgm->get("Player Respawning") == null) {
+                    $pre->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Player Respawning")));
+                }
+                    $pre->setCancelled();
             }
         }
     }
     public function onToggleSneak (PlayerToggleSneakEvent $ptse){
-        if ($this->cfg->get("Sneaking") == false or $this->cfg->get("Sneaking") == disabled) {
+        if ($this->cfg->get("Sneaking") == false or $this->cfg->get("Sneaking") == disable) {
             if (!$pre->getPlayer()->hasPermission('servermanager.bypass')) {
-                $pre->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Sneaking")));
-                $pre->setCancelled();
+                if(!$this->cfgm->get("Sneaking") == null) {
+                    $pre->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Sneaking")));
+                }
+                    $pre->setCancelled();
             }
         }
     }
     public function onAchievementAwarded (PlayerAchievementAwardedEvent $paae){
-        if ($this->cfg->get("Achievements") == false or $this->cfg->get("Achievements") == disabled) {
+        if ($this->cfg->get("Achievements") == false or $this->cfg->get("Achievements") == disable) {
             if (!$paae->getPlayer()->hasPermission('servermanager.bypass')) {
-                $paae->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Achievements")));
+                if(!$this->cfgm->get("Achievements") == null) {
+                    $paae->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Achievements")));
+                }
                 $paae->setCancelled();
             }
         }
     }
     public function onExplosionsPrime (ExplosionPrimeEvent $epe){
-        if ($this->cfg->get("Explosions") == false or $this->cfg->get("Explosions") == disabled) {
+        if ($this->cfg->get("Explosions") == false or $this->cfg->get("Explosions") == disable) {
             if (!$epe->getPlayer()->hasPermission('servermanager.bypass')) {
-                $epe->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Explosions")));
-                $epe->setCancelled();
+                if(!$this->cfgm->get("Explosions") == null) {
+                    $epe->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Explosions")));
+                }
+                    $epe->setCancelled();
             }
         }
     }
     public function onOpen (InventoryOpenEvent $ioe){
-        if ($this->cfg->get("Inventory Open") == false or $this->cfg->get("Inventory Open") == disabled) {
+        if ($this->cfg->get("Inventory Open") == false or $this->cfg->get("Inventory Open") == disable) {
             if (!$ioe->getPlayer()->hasPermission('servermanager.bypass')) {
-                $ioe->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Inventory Open")));
-                $ioe->setCancelled();
+                if(!$this->cfgm->get("Inventory Open") == null) {
+                    $ioe->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Inventory Open")));
+                }
+                    $ioe->setCancelled();
             }
         }
     }
     public function onPickupItem (InventoryPickupItemEvent $ipie){
-        if ($this->cfg->get("Item Pickup") == false or $this->cfg->get("Item Pickup") == disabled) {
+        if ($this->cfg->get("Item Pickup") == false or $this->cfg->get("Item Pickup") == disable) {
             if (!$ipie->getPlayer()->hasPermission('servermanager.bypass')) {
-                $ipie->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Item Pickup")));
-                $ipie->setCancelled();
+                if(!$this->cfgm->get("Item Pickup") == null) {
+                    $ipie->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Item Pickup")));
+                }
+                    $ipie->setCancelled();
             }
         }
     }
     public function onCraftItem (CraftItemEvent $cie){
-        if ($this->cfg->get("Item Crafting") == false or $this->cfg->get("Item Crafting") == disabled) {
+        if ($this->cfg->get("Item Crafting") == false or $this->cfg->get("Item Crafting") == disable) {
             if (!$cie->getPlayer()->hasPermission('servermanager.bypass')) {
-                $cie->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Item Crafting")));
+                if(!$this->cfgm->get("Item Crafting") == null) {
+                    $cie->getPlayer()->sendMessage($prefix2 . " : " . str_replace("&", "§", $this->cfgm->get("Item Crafting")));
+                }
                 $cie->setCancelled();
             }
         }
@@ -210,6 +244,6 @@ class ServerManager extends PluginBase implements Listener
 
     public function onDisable()
     {
-        $this->getLogger()->info($prefix . SM::RED . "has been disabled");
+        $this->getLogger()->info($prefix . SM::RED . "has been disable");
     }
 }
