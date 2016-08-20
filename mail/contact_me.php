b9918188efedb99b5b5e1295f74ed1b9f478eeb4
Amir Emad <amir.emad9@gmail.com>
2016-08-20 11:59:04 -0700

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -12,13 +12,29 @@ if(empty($_POST['name'])  		||
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
-	
-// Create the email and send the message
-$to = 'support@mcrafters.ml'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@mcrafters.ml\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+$from = 'contact@mcrafters.ml';
+$to = 'amir.emad9@gmail.com';
+$subject = "ServerManager Contact Form:  $name";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";;
+
+$headers = array(
+    'From' => $from,
+    'To' => $to,
+    'Subject' => $subject
+);
+
+$smtp = Mail::factory('smtp', array(
+        'host' => 'mail.mcrafters.ml',
+        'auth' => true,
+        'username' => 'contact@mcrafters.ml',
+        'password' => 'mcrafters12'
+    ));
+
+$mail = $smtp->send($to, $headers, $body);
+
+if (PEAR::isError($mail)) {
+    echo('<p>' . $mail->getMessage() . '</p>');
+} else {
+    echo('<p>Message successfully sent!</p>');
+}			
 ?>
